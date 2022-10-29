f11bd4603851d8d35a652b4f9d64f01a51776daa
Gonzalo Ebani <gonzaloebani10@gmail.com>
2022-10-29 20:05:52 -0300

    entrega tercer desafio (wip)

---
+++ app.js
@@ -0,0 +1,163 @@
+let carrito = [];
+let stock = [];
+
+const tabla = document.getElementById("items");
+const agregar = document.querySelector("#agregar");
+const descuento = document.querySelector("#descuento");
+const ordenar = document.getElementById("ordenar");
+const vaciar = document.getElementById("vaciar");
+const productosEnStock = document.getElementById("productos");
+
+
+stock.push(new Producto("Remera", 1000));
+stock.push(new Producto("Jean", 1700));
+stock.push(new Producto("Vestido", 1500));
+stock.push(new Producto("Medias", 400));
+
+
+stock.forEach((producto) => {
+    let option = document.createElement("option");
+    option.innerText = `${producto.nombre} $${producto.precio}`
+    option.value = stock.indexOf(producto); ///nos devuelve el indice en el array
+    productosEnStock.appendChild(option); ///le agregamos al select el option
+})
+
+
+function newRow(item) {
+    let row = document.createElement("tr");
+    let pos = carrito.indexOf(item); ///el indice en el carrito
+
+    ///creo celda con NOMBRE
+    let celda = document.createElement("td");
+    celda.innerText = item.producto.nombre;
+    row.append(celda); ///agregue el nombre
+
+    ///creo la celda CANTIDAD
+    celda = document.createElement("td");
+    celda.innerText = item.cantidad;
+
+    ///le agrego los botones INCREMENTO y DECREMENTO
+    let botonIncremento = document.createElement("button");
+    botonIncremento.className = "btn btn-primary";
+    botonIncremento.innerText = "+";
+
+    let botonDecremento = document.createElement("button");
+    botonDecremento.className = "btn btn-primary";
+    botonDecremento.innerText = "-";
+
+    botonIncremento.onclick = () =>
+    {
+        carrito[pos].cantidad++;
+        listadoUpdate();
+        localStorage.setItem("carrito", JSON.stringify(carrito));
+    }
+
+    botonDecremento.onclick = () =>
+    {
+        if (carrito[pos].cantidad > 0)
+        {
+            carrito[pos].cantidad--;
+            listadoUpdate();
+            localStorage.setItem("carrito",JSON.stringify(carrito));
+        }
+    }
+
+    celda.append(botonIncremento);
+    celda.append(botonDecremento);
+    row.append(celda);
+
+    ///creo la celda precio
+    celda = document.createElement("td");
+    celda.innerText = item.producto.precio;
+    row.append(celda);
+
+    ///Creacion del boton Eliminar
+    let botonEliminar = document.createElement('button');
+    botonEliminar.className = "btn btn-danger";
+    botonEliminar.innerText = "Eliminar";
+    
+    botonEliminar.onclick = () => {
+        carrito.splice(pos,1); ///elimina el objeto en la posicion pos del carrito
+        listadoUpdate(); //vuelvo a generar los elementos del DOM
+        localStorage.setItem("carrito",JSON.stringify(carrito));
+    }
+
+    celda = document.createElement("td");
+    celda.append(botonEliminar);
+    row.append(celda);
+    tabla.append(row);
+
+}
+/* calculo el total de precios */
+function calculoTotal()
+{
+     //Acumulo PrecioTotal
+     total = document.getElementById("total");
+     total.innerText = carrito.reduce(
+         (suma,item) => 
+         suma + item.producto.precio*item.cantidad
+     ,0);
+}
+
+
+/* Actualizacion de la tabla */
+function listadoUpdate() {
+    tabla.innerHTML = "";
+    carrito.forEach((item) => 
+    {
+        newRow(item);
+    });
+    calculoTotal();
+}
+
+/* Se agrega un nuevo Item al carrito  */
+agregar.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let producto = stock[productosEnStock.value];
+
+    let nuevoElementoCarrito = new Item(producto,1); ///creamos el item de carrito
+    carrito.push(nuevoElementoCarrito); ///agrego el elemento al carrito
+    newRow(nuevoElementoCarrito);
+    calculoTotal();
+    localStorage.setItem("carrito",JSON.stringify(carrito));
+    
+});
+/* aplicamos el 20% de descuentoa los productos, si el cupon ingresado es valido */
+descuento.addEventListener("submit", (e) => {
+   e.preventDefault();
+   let cupon = document.getElementById("cuponDescuento").value;
+   if (cupon == 'cursojs2022') 
+   {
+    carrito = carrito.map((item) => {
+        return new Item(
+            new Producto(
+                item.producto.nombre,
+                (item.producto.precio - (item.producto.precio * 0.20))
+            ),
+            item.cantidad
+        );
+    });
+    listadoUpdate();
+   }
+   else {
+    alert ("Cupon no valido");
+   }
+   localStorage.setItem("carrito", JSON.stringify(carrito));
+});
+
+/* Ordenamiento por nombre */
+ordenar.onclick = () => {
+    carrito.sort((actual,siguiente) => {
+        return actual.producto.nombre.localeCompare(siguiente.producto.nombre); 
+    })
+    listadoUpdate();
+};
+
+/* Vaciar carrito */
+vaciar.onclick = () => {
+  carrito  = [];
+  listadoUpdate();
+  localStorage.setItem("carrito",JSON.stringify(carrito));
+};
+
+
