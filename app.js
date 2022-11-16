05241fe8bd3727bccc663ac76ef05deeb83526d1
Gonzalo Ebani <gonzaloebani10@gmail.com>
2022-11-15 23:33:25 -0300

    Entrega Proyecto Final

--- app.js
+++ app.js
@@ -10,155 +10,161 @@ const productosEnStock = document.getElementById("productos");
 
 listadoUpdate();
 
-stock.push(new Producto("Remera", 1000));
-stock.push(new Producto("Jean", 1700));
-stock.push(new Producto("Vestido", 1500));
-stock.push(new Producto("Medias", 400));
+fetch(`./stock.json`)
+  .then((response) => {
+    return response.text();
+  })
+  .then((data) => {
+    stock = JSON.parse(data);
+    cargarListado();
+    console.log(stock);
+  });
 
+console.log(stock);
 
-stock.forEach((producto) => {
+function cargarListado() {
+  stock.forEach((producto) => {
     let option = document.createElement("option");
-    option.innerText = `${producto.nombre} $${producto.precio}`
+    option.innerText = `${producto.nombre} $${producto.precio}`;
     option.value = stock.indexOf(producto); ///nos devuelve el indice en el array
     productosEnStock.appendChild(option); ///le agregamos al select el option
-});
+  });
+}
 
 function newRow(item) {
-    let row = document.createElement("tr");
-    let pos = carrito.indexOf(item); ///el indice en el carrito
+  let row = document.createElement("tr");
+  let pos = carrito.indexOf(item); ///el indice en el carrito
 
-    ///creo celda con NOMBRE
-    let celda = document.createElement("td");
-    celda.innerText = item.producto.nombre;
-    row.append(celda); ///agregue el nombre
+  ///creo celda con NOMBRE
+  let celda = document.createElement("td");
+  celda.innerText = item.producto.nombre;
+  row.append(celda); ///agregue el nombre
 
-    ///creo la celda CANTIDAD
-    celda = document.createElement("td");
-    celda.innerText = item.cantidad;
+  ///creo la celda CANTIDAD
+  celda = document.createElement("td");
+  celda.innerText = item.cantidad;
 
-    ///le agrego los botones INCREMENTO y DECREMENTO
-    let botonIncremento = document.createElement("button");
-    botonIncremento.className = "btn btn-primary";
-    botonIncremento.innerText = "+";
+  ///le agrego los botones INCREMENTO y DECREMENTO
+  let botonIncremento = document.createElement("button");
+  botonIncremento.className = "btn btn-primary";
+  botonIncremento.innerText = "+";
 
-    let botonDecremento = document.createElement("button");
-    botonDecremento.className = "btn btn-primary";
-    botonDecremento.innerText = "-";
+  let botonDecremento = document.createElement("button");
+  botonDecremento.className = "btn btn-primary";
+  botonDecremento.innerText = "-";
 
-    botonIncremento.onclick = () =>
-    {
-        carrito[pos].cantidad++;
-        listadoUpdate();
-        localStorage.setItem("carrito", JSON.stringify(carrito));
+  botonIncremento.onclick = () => {
+    carrito[pos].cantidad++;
+    listadoUpdate();
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+  };
+
+  botonDecremento.onclick = () => {
+    if (carrito[pos].cantidad > 0) {
+      carrito[pos].cantidad--;
+      listadoUpdate();
+      localStorage.setItem("carrito", JSON.stringify(carrito));
     }
+  };
 
-    botonDecremento.onclick = () =>
-    {
-        if (carrito[pos].cantidad > 0)
-        {
-            carrito[pos].cantidad--;
-            listadoUpdate();
-            localStorage.setItem("carrito",JSON.stringify(carrito));
-        }
-    }
+  celda.append(botonIncremento);
+  celda.append(botonDecremento);
+  row.append(celda);
 
-    celda.append(botonIncremento);
-    celda.append(botonDecremento);
-    row.append(celda);
+  ///creo la celda precio
+  celda = document.createElement("td");
+  celda.innerText = item.producto.precio;
+  row.append(celda);
 
-    ///creo la celda precio
-    celda = document.createElement("td");
-    celda.innerText = item.producto.precio;
-    row.append(celda);
+  ///Creacion del boton Eliminar
+  let botonEliminar = document.createElement("button");
+  botonEliminar.className = "btn btn-danger";
+  botonEliminar.innerText = "Eliminar";
 
-    ///Creacion del boton Eliminar
-    let botonEliminar = document.createElement('button');
-    botonEliminar.className = "btn btn-danger";
-    botonEliminar.innerText = "Eliminar";
-    
-    botonEliminar.onclick = () => {
-        carrito.splice(pos,1); ///elimina el objeto en la posicion pos del carrito
-        listadoUpdate(); //vuelvo a generar los elementos del DOM
-        localStorage.setItem("carrito",JSON.stringify(carrito));
-    }
+  botonEliminar.onclick = () => {
+    carrito.splice(pos, 1); ///elimina el objeto en la posicion pos del carrito
+    listadoUpdate(); //vuelvo a generar los elementos del DOM
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+  };
 
-    celda = document.createElement("td");
-    celda.append(botonEliminar);
-    row.append(celda);
-    tabla.append(row);
-
+  celda = document.createElement("td");
+  celda.append(botonEliminar);
+  row.append(celda);
+  tabla.append(row);
 }
 /* calculo el total de precios */
-function calculoTotal()
-{
-     //Acumulo PrecioTotal
-     total = document.getElementById("total");
-     total.innerText = carrito.reduce(
-         (suma,item) => 
-         suma + item.producto.precio*item.cantidad
-     ,0);
+function calculoTotal() {
+  //Acumulo PrecioTotal
+  total = document.getElementById("total");
+  total.innerText = carrito.reduce(
+    (suma, item) => suma + item.producto.precio * item.cantidad,
+    0
+  );
 }
-
 
 /* Actualizacion de la tabla */
 function listadoUpdate() {
-    tabla.innerHTML = "";
-    carrito.forEach((item) => 
-    {
-        newRow(item);
-    });
-    calculoTotal();
+  tabla.innerHTML = "";
+  carrito.forEach((item) => {
+    newRow(item);
+  });
+  calculoTotal();
 }
 
 /* Se agrega un nuevo Item al carrito  */
 agregar.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let producto = stock[productosEnStock.value];
+  e.preventDefault();
+  let producto = stock[productosEnStock.value];
 
-    let nuevoElementoCarrito = new Item(producto,1); ///creamos el item de carrito
+  let index = carrito.findIndex((element) => element.producto.nombre == producto.nombre);
+  if (index != -1) {
+    carrito[index].cantidad += 1;
+    listadoUpdate();
+  } else {
+    let nuevoElementoCarrito = new Item(producto, 1); ///creamos el item de carrito
     carrito.push(nuevoElementoCarrito); ///agrego el elemento al carrito
     newRow(nuevoElementoCarrito);
-    calculoTotal();
-    localStorage.setItem("carrito",JSON.stringify(carrito));
-    
+  }
+  calculoTotal();
+  localStorage.setItem("carrito", JSON.stringify(carrito));
 });
+
 /* aplicamos el 20% de descuentoa los productos, si el cupon ingresado es valido */
 descuento.addEventListener("submit", (e) => {
-   e.preventDefault();
-   let cupon = document.getElementById("cuponDescuento").value;
-   if (cupon == 'cursojs2022') 
-   {
+  e.preventDefault();
+  let cupon = document.getElementById("cuponDescuento").value;
+  if (cupon == "cursojs2022") {
     carrito = carrito.map((item) => {
-        return new Item(
-            new Producto(
-                item.producto.nombre,
-                (item.producto.precio - (item.producto.precio * 0.20))
-            ),
-            item.cantidad
-        );
+      return new Item(
+        new Producto(
+          item.producto.nombre,
+          item.producto.precio - item.producto.precio * 0.2
+        ),
+        item.cantidad
+      );
     });
     listadoUpdate();
-   }
-   else {
-    alert ("Cupon no valido");
-   }
-   localStorage.setItem("carrito", JSON.stringify(carrito));
+  } else {
+    Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Cupon no valido!',
+      })
+  }
+  localStorage.setItem("carrito", JSON.stringify(carrito));
 });
 
 /* Ordenamiento por nombre */
 ordenar.onclick = () => {
-    carrito.sort((actual,siguiente) => {
-        return actual.producto.nombre.localeCompare(siguiente.producto.nombre); 
-    })
-    listadoUpdate();
+  carrito.sort((actual, siguiente) => {
+    return actual.producto.nombre.localeCompare(siguiente.producto.nombre);
+  });
+  listadoUpdate();
 };
 
 /* Vaciar carrito */
 vaciar.onclick = () => {
-  carrito  = [];
+  carrito = [];
   listadoUpdate();
-  localStorage.setItem("carrito",JSON.stringify(carrito));
+  localStorage.setItem("carrito", JSON.stringify(carrito));
 };
-
-
-
